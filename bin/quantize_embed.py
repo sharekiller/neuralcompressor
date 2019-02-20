664c70ae008dfce76ad7826dec6483ff82fb6b71
sharekiller <sharekiller@aliyun.com>
2019-02-20 15:11:29 +0800

    Update quantize_embed.py

--- bin/quantize_embed.py
+++ bin/quantize_embed.py
@@ -1,34 +1,29 @@
 from __future__ import absolute_import, division, print_function
-
-import sys
-sys.path.append(".")
-import os
-import time
 import numpy as np
 from argparse import ArgumentParser
-from nncompress import EmbeddingCompressor
+import sys
+
+sys.argv = ["convert_glove2numpy.py", r"D:\Work\research\text_classification\projects\model_compress\neuralcompressor-master\data\glove.6B.300d.txt"]
 
 if __name__ == '__main__':
     ap = ArgumentParser()
-    ap.add_argument("--matrix", default="data/glove.6B.300d.npy")
-    ap.add_argument("--model", default="data/mymodel")
-    ap.add_argument("--limit", default=50000, type=int)
-    ap.add_argument("-M", "--M", default=32, type=int)
-    ap.add_argument("-K", "--K", default=16, type=int)
-    ap.add_argument("--train", action="store_true")
-    ap.add_argument("--export", action="store_true")
-    ap.add_argument("--evaluate", action="store_true")
+    ap.add_argument("path")
+    ap.add_argument("--dim", default=300, type=int, help="dimension")
     args = ap.parse_args()
 
-    matrix = np.load(args.matrix)
-    if args.limit > 0:
-        matrix = matrix[:args.limit]
+    glove_path = args.path
+    embed_path = args.path.replace(".txt", ".npy")
+    word_path = args.path.replace(".txt", ".word")
+    f_wordout = open(word_path, "w", encoding='UTF-8')
+    print("convert {} to {}".format(glove_path, embed_path))
 
-    compressor = EmbeddingCompressor(args.M, args.K, args.model)
-    if args.train:
-        compressor.train(matrix)
-    elif args.export:
-        compressor.export(matrix, args.model)
-    elif args.evaluate:
-        distance = compressor.evaluate(matrix)
-        print("Mean euclidean distance:", distance)
+    lines = list(open(glove_path,'r',encoding='UTF-8'))
+    embed_matrix = np.zeros((len(lines), args.dim), dtype='float32')
+    for i, line in enumerate(lines):
+        parts = line.rstrip().split(' ')
+        word = parts[0]
+        vec = np.array(parts[1:], dtype='float32')
+        embed_matrix[i] = vec
+        f_wordout.write(word + "\n")
+    np.save(embed_path, embed_matrix)
+
